96b3f5608d8f3af1c725976b8b816a052248f8b7
Jamey Jones <jpkey764@gmail.com>
2025-05-24 15:17:07 -0400

    added NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { usePage } from "./layout/PageContext";
+
 
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
 import useQuery from "./api/useQuery";
+import { Route,Routes } from "react-router";
+import Layout from "./layout/Layout.jsx";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -13,11 +15,18 @@ import useQuery from "./api/useQuery";
  */
 export default function App() {
  
-  const { page } = usePage();
 
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
+  <Routes>
+    <Route>
+      <Route element = {<Layout/>}/>
+      <Route path ="/" element ={<Register/>}/>
+      <Route path="/login" element ={<Login/>}/>
+      <Route path= "/activities" element={<ActivitiesPage/>}/> 
+      <Route path="*" element={<Error404/>}/>
+    </Route>
+
+  </Routes>
+
 
  
   return <Error404 />;
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,23 +1,28 @@
-import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+import { NavLink } from "react-router";
 
 /** Navbar with site navigation links */
-export default function Navbar() {
+{
   const { token, logout } = useAuth();
   const { setPage } = usePage();
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
-        {token ? (
-          <a onClick={() => logout()}>Log out</a>
-        ) : (
-          <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
-          </>
-        )}
+        <NavLink to = "/" end>
+        Activities
+        </NavLink>
+
+        
+          <NavLink to = "/logout" end>
+          Log out
+          </NavLink>
+          
+            <NavLink to = "/register" end>
+            Register
+            </NavLink>
+
+            <NavLink to ="/login"> Login </NavLink>
+          
       </nav>
     </header>
   );
